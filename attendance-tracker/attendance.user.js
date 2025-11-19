6bdd2338a5af818c0ee7d7662282deae9395ac6e
X445 <dhrubajyoti.mandal@lgepartner.com>
2025-11-19 15:00:04 +0530

    updated download/update url

--- attendance-tracker/attendance.user.js
+++ attendance-tracker/attendance.user.js
@@ -5,6 +5,8 @@
 // @description  try to take over the world!
 // @author       X445
 // @match        http://si-rd10-intdbsr/Attendance/EmpAttendanceReport.aspx
+// @updateURL    https://raw.githubusercontent.com/x445ulge/userscripts/refs/heads/main/attendance-tracker/attendance.user.js
+// @downloadURL  https://raw.githubusercontent.com/x445ulge/userscripts/refs/heads/main/attendance-tracker/meta.js
 // @grant        none
 // ==/UserScript==
 
--- attendance-tracker/meta.js
+++ attendance-tracker/meta.js
@@ -5,5 +5,7 @@
 // @description  try to take over the world!
 // @author       X445
 // @match        http://si-rd10-intdbsr/Attendance/EmpAttendanceReport.aspx
+// @updateURL    https://raw.githubusercontent.com/x445ulge/userscripts/refs/heads/main/attendance-tracker/attendance.user.js
+// @downloadURL  https://raw.githubusercontent.com/x445ulge/userscripts/refs/heads/main/attendance-tracker/meta.js
 // @grant        none
 // ==/UserScript==
